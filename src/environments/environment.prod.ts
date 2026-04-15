7687ad1247aafc653730c06ffb6b07089c24588d
Daniel Figueroa <danielfigueroa2611@gmail.com>
2026-04-14 21:34:55 -0500

    Creación - modulos - core y shared, configuración - environment - firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  fireBaseConfig : {
+    apiKey: "AIzaSyBONcPYmqWXsoVZWlpj4AyAXASEsTs1NIs",
+    authDomain: "my-digital-wallet-965f0.firebaseapp.com",
+    projectId: "my-digital-wallet-965f0",
+    storageBucket: "my-digital-wallet-965f0.firebasestorage.app",
+    messagingSenderId: "317799917374",
+    appId: "1:317799917374:web:6ab24c9be6c15c47402e6f"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  fireBaseConfig : {
+    apiKey: "AIzaSyBONcPYmqWXsoVZWlpj4AyAXASEsTs1NIs",
+    authDomain: "my-digital-wallet-965f0.firebaseapp.com",
+    projectId: "my-digital-wallet-965f0",
+    storageBucket: "my-digital-wallet-965f0.firebasestorage.app",
+    messagingSenderId: "317799917374",
+    appId: "1:317799917374:web:6ab24c9be6c15c47402e6f"
+  }
 };
 
 /*
